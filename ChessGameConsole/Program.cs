ab01b67ab22ef51df7b99e5cd8aa720f86240196
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2020-02-22 18:59:14 -0300

    Melhorando a implementação do método tabuleiro.

--- ChessGameConsole/Program.cs
+++ ChessGameConsole/Program.cs
@@ -7,8 +7,11 @@ namespace ChessGameConsole
     {
         static void Main()
         {
-            ChessPosition position = new ChessPosition('c', 7);
-            System.Console.WriteLine(position.ToPosition());
+            ChessBoard board = new ChessBoard();
+            board.PutPiece(new Position(4, 0), new King(Color.Black, board));
+            board.PutPiece(new Position(0, 0), new Tower(Color.White, board));
+            board.PutPiece(new Position(7, 0), new Tower(Color.White, board));
+            Screen.DrawBoard(board);
         }
     }
 }
--- ChessGameConsole/Screen.cs
+++ ChessGameConsole/Screen.cs
@@ -7,9 +7,10 @@ namespace ChessGameConsole
     {
         public static void DrawBoard(ChessBoard board)
         {
-            for(int column = 0; column < ChessBoard.rows; column++)
+            for(int column = 0; column < ChessBoard.columns; column++)
             {
-                for(int row = 0; row < ChessBoard.columns; row++)
+                Console.Write($"{ChessBoard.rows - column} ");
+                for(int row = 0; row < ChessBoard.rows; row++)
                 {
                     Piece piece = board.Find(row, column);
                     if (piece == null)
@@ -18,10 +19,27 @@ namespace ChessGameConsole
                     }
                     else
                     {
-                        Console.Write($"{piece} ");
+                        DrawPiece(piece);
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        private static void DrawPiece(Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write(piece);
+            } 
+            else
+            {
+                ConsoleColor temp = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = temp;
             }
         }
     }
