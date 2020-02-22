59ef988c6ec61ba1a55d542852c720221b55c147
Leonardo Rangel dos Santos <leonardorangel2604@gmail.com>
2020-02-22 17:44:59 -0300

    Criando a classe ChessPosition

--- ChessGameConsole/Program.cs
+++ ChessGameConsole/Program.cs
@@ -7,11 +7,8 @@ namespace ChessGameConsole
     {
         static void Main()
         {
-            ChessBoard board = new ChessBoard();
-            board.PutPiece(new Position(4, 0), new King(Color.White, board));
-            board.PutPiece(new Position(0, 0), new Tower(Color.White, board));
-            board.PutPiece(new Position(7, 0), new Tower(Color.White, board));
-            Screen.DrawBoard(board);
+            ChessPosition position = new ChessPosition('c', 7);
+            System.Console.WriteLine(position.ToPosition());
         }
     }
 }
